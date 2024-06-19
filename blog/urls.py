46c989c8941a02921bad61220c4ddb0ff1b2012d
SyncMohsenNemati <mohsen_nmati@gmail.com>
2024-06-19 18:57:00 +0330

    updating urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('posts/<slug:slug>',views.posts,name='posts'),
+    path('posts/',views.posts,name='posts'),
     path('single-post/',views.single_post,name='post-details')
 ]
