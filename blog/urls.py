c4199d2abb858d260b8f9e8d0baa5e796fa9125d
SyncMohsenNemati <mohsen_nmati@gmail.com>
2024-06-20 12:20:04 +0330

    updating urls.py and views.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('posts/',views.posts,name='posts'),
-    path('posts/single-post/',views.single_post,name='post-details')
+    path('posts/<slug:slug>',views.single_post,name='post-details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404, redirect
 from django.http import HttpResponse
+
 
 
 # Create your views here.
@@ -12,5 +13,10 @@ def posts(request):
     return render(request, 'blog/AllPost.html')
 
 
-def single_post(request):
+def single_post(request, slug):
+    # post = get_object_or_404(index() , slug = slug)
+    # context = {
+    #     "post_title": post.title
+    # }
+    # return  render(request , 'blog/singlePost.html' , context)
     return render(request, 'blog/singlePost.html')
