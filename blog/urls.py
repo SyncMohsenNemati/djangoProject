32c161c6f4a5c75de83bdf398046f17e48214760
SyncMohsenNemati <mohsen_nmati@gmail.com>
2024-06-20 11:52:17 +0330

    updating urls.py and views.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('posts/',views.posts,name='posts'),
-    path('single-post/',views.single_post,name='post-details')
+    path('posts/single-post/',views.single_post,name='post-details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
 
+
 def posts(request):
-    return  render(request,'blog/AllPost.html')
+    return render(request, 'blog/AllPost.html')
+
 
 def single_post(request):
-    pass
+    return render(request, 'blog/singlePost.html')
